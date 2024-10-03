9545735e95537549f12feb9951df2a7724362a66
ArmandoxxXD <mamojose58@gmail.com>
2024-10-03 03:14:33 -0600

    first commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * Autor: Jose Armando Gutierrez Rodriguez
+ * Fecha: 03/10/2024
+ * Prototype 1
+ * **/
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 20;
+    // Start is called before the first frame update
+    // Update the position of the game object every frame based on its current position and the direction of movement (forward) multiplied by the time elapsed since the last frame.
+    float inputY;
+    float inputX;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputY = Input.GetAxis("Vertical");
+        inputX = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, Time.deltaTime*10*inputX);
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*inputY);
+    }
+}
